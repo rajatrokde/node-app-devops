cefefff429ef341c81d6b5d54e77bf6a28ac6c01
Rajat R Rokde <116082212+rajatrokde@users.noreply.github.com>
2026-03-29 17:37:06 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,9 +8,8 @@ data "aws_ssm_parameter" "ubuntu" {
 
 resource "aws_instance" "devops_server" {
   ami           = data.aws_ssm_parameter.ubuntu.value
-  instance_type = "t2.micro"
-
+  instance_type = var.instance_type
   tags = {
-    Name = "DevOps-Server"
+    Name = var.instance_name
   }
 }

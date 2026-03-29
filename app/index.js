db6c1f70294289073a814b27669933a7b15ccbdc
root <root@Rajat.localdomain>
2026-03-29 08:42:12 +0000

    add

---
+++ app/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+// Health check endpoint (important for DevOps / monitoring)
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: "UP" });
+});
+
+// Main route
+app.get('/', (req, res) => {
+  res.send("🚀 DevOps Node App Running Successfully using express!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

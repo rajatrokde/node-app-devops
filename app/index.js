21bc702435461f6e6fb752538ddf7d7539d12899
Rajat R Rokde <116082212+rajatrokde@users.noreply.github.com>
2026-03-29 15:54:47 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -10,7 +10,7 @@ app.get('/health', (req, res) => {
 
 // Main route
 app.get('/', (req, res) => {
-  res.send("🚀 DevOps Node App Running Successfully using express!");
+  res.send("🚀 DevOps Node App Running Successfully using express !");
 });
 
 app.listen(PORT, () => {

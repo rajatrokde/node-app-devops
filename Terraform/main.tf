57b90e882237eee2b186caec49a10fc4183a7b94
Rajat R Rokde <116082212+rajatrokde@users.noreply.github.com>
2026-03-29 17:16:56 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,20 +2,13 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["459798871689"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
+data "aws_ssm_parameter" "ubuntu" {
+  name = "/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
 }
 
-
 resource "aws_instance" "devops_server" {
-  ami           = data.aws_ami.ubuntu.id   
-  instance_type = "c7i-flex.large"
+  ami           = data.aws_ssm_parameter.ubuntu.value
+  instance_type = "t2.micro"
 
   tags = {
     Name = "DevOps-Server"

220a546ebd3e728ed0e6db4ad4a3b8b8056d8a9e
Rajat R Rokde <116082212+rajatrokde@users.noreply.github.com>
2026-04-13 20:29:07 +0530

    Update success message in main route

--- app/index.js
+++ app/index.js
@@ -10,7 +10,7 @@ app.get('/health', (req, res) => {
 
 // Main route
 app.get('/', (req, res) => {
-  res.send("🚀 Boom DevOps Node App Running Successfully using express !");
+  res.send("🚀 Boom DevOps Node App Running Successfully using express project by RAJAT !");
 });
 
 app.listen(PORT, () => {
